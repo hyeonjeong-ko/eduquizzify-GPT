a664243d905bdf8acd35c505d104179daa928c6e
hyeonjeong-ko <hyeonkjjang@gmail.com>
2024-09-14 15:32:01 +0900

    fix: Remove mistakenly uploaded files

--- api/v1/file_upload/main.py
+++ api/v1/file_upload/main.py
@@ -5,8 +5,8 @@ from quiz import router as quiz_router
 app = FastAPI()
 
 # 두 개의 라우터를 메인 앱에 등록
-app.include_router(summarize_router, prefix="/files")  # 여기서 /files로 요약 관련 기능을 처리
-app.include_router(quiz_router, prefix="/files")  # 여기서 /files로 요약 관련 기능을 처리
+app.include_router(summarize_router, prefix="/files")
+app.include_router(quiz_router, prefix="/files")
 
 if __name__ == "__main__":
     import uvicorn
